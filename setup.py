b4d62eb88485993e63a27b22e8fedfe3c47fa33a
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2016-01-10 19:58:05 -0200

    Added setup.py and the MIT license.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='formscribe',
+    packages=['formscribe'],
+    version='0.1',
+    description='A practical form validation and submitting library',
+    author='Martin Jungblut Schreiner',
+    author_email='martinjungblut@gmail.com',
+    url='https://github.com/martinjungblut/formscribe',
+    download_url='https://github.com/martinjungblut/formscribe/tarball/0.1',
+    keywords=['form', 'validation'],
+    license='MIT'
+)
